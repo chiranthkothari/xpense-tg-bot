653dd417aac05bf255206d4c12cbd449a7ae85f6
Chiranth Jain <chiranth.k@logically.co.uk>
2024-01-14 15:08:16 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,31 @@
+from http import client
+from unicodedata import category
+from fastapi import FastAPI
+from fastapi.encoders import jsonable_encoder
+from fastapi.responses import JSONResponse
+from pydantic import BaseModel
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+
+client = gspread.service_account("creds.json")
+sheet = client.open_by_url(
+    "https://docs.google.com/spreadsheets/d/1virgIh_FdgZHYLpIopt41uZ-HhQQUxYdOeKnoZp_9w8/").sheet1
+
+
+app = FastAPI()
+
+
+class Record(BaseModel):
+    type: str
+    description: str
+    date: str
+    amount: int
+    category: str
+
+
+@app.post("/track/")
+def save_to_gsheet(record: Record):
+    row = [record.type, record.description, record.date, record.amount, record.category]
+    index = 2  # Rows start at index 1, and we skip the header row
+    sheet.append_row(row, index)  # Insert the row at the correct index
+    return JSONResponse(status_code=200, content=jsonable_encoder({"message": f"Saved {record.description}"}), media_type="application/json")
